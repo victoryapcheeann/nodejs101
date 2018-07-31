be60385ec373a605fad57fe66a35ae7fb4548886
Victor Yap <victoryapcheeann@gmail.com>
2018-07-31 12:35:24 +0800

    Starting on express

--- 2server.js
+++ 2server.js
@@ -55,7 +55,7 @@ console.log('server is running on port 8181')
 
 
 
- //10.4) Json response
+/* //10.4) Json response
  //API - Application Programming Interface - Provide tools we can use to build our own software
  const http = require('http');
  const fs = require('fs');
@@ -73,4 +73,34 @@ console.log('server is running on port 8181')
 
  server.listen(8181, '127.0.0.1');
  console.log('server is running on port 8181')
-//10.3 End */
+//10.4 End */
+
+
+
+ /* //10.5) Routing
+ const http = require('http');
+ const fs = require('fs');
+
+ const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-type': 'text/html'}); 
+        let HTML = fs.readFileSync('./2index.html')
+        res.end(HTML)
+    } else if (req.url === '/api/user') {
+        res.writeHead(200, {'Content-type': 'text/application/json'}); 
+        const json = JSON.stringify({
+            name:'john',
+            cars:['Ford','Tesla']
+        })
+        res.end(json);
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+ })
+
+ server.listen(8181, '127.0.0.1');
+ console.log('server is running on port 8181')
+//10.5 End */
+
+
